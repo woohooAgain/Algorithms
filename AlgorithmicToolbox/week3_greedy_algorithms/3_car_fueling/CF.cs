462e2e8ce32c699bf89d95d9296dcfcdcc566566
Ilya.Kulikov <ilia.s.kulikov@gmail.com>
2019-09-07 11:42:24 +0300

    week3 Car Fueling

--- AlgorithmicToolbox/week3_greedy_algorithms/3_car_fueling/CF.cs
+++ AlgorithmicToolbox/week3_greedy_algorithms/3_car_fueling/CF.cs
@@ -11,62 +11,43 @@ namespace CarFueling
             var totalWay = Console.ReadLine();
             var maxDistanceOnFullTank = Console.ReadLine();
             var numberOfStations = Console.ReadLine();
-            var stationDistancesString = Console.ReadLine();
-            var result = 0;
+            var stationDistancesString = Console.ReadLine();            
             var stationDistancesList = stationDistancesString.Split(' ').Select(d => int.Parse(d)).ToList();
-            //stationDistancesList.Add(int.Parse(totalWay));
-            try
-            {
-                var distances = CountDistancesBetweenStations(stationDistancesList, int.Parse(totalWay), int.Parse(maxDistanceOnFullTank));
-                result = CountStopsNumber(distances, int.Parse(maxDistanceOnFullTank));
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine($"{ex}");
-                result = -1;
-            }
+            stationDistancesList.Add(int.Parse(totalWay));
+            stationDistancesList.Insert(0, 0);
+            var result = CountStopsNumber(int.Parse(maxDistanceOnFullTank), stationDistancesList, 0);
             
             Console.WriteLine($"{result}");
         }
-
-        static List<int> CountDistancesBetweenStations(List<int> stations, int totalWay, int fullTankDistance)
+        static int CountStopsNumber(int maxDistance, List<int> stations, int currentPoint)
         {
-            var result = new List<int>(stations.Count()+1);
-            var localStations = new List<int>(stations);
-            localStations.Insert(0,0);
-            localStations.Add(totalWay);
-            for(var i = 0; i < localStations.Count() - 1; i++)
+            var stops = 0;
+            for(var i = currentPoint; i < stations.Count(); i++)
             {
-                Console.WriteLine($"{i} {localStations[i]} {localStations[i+1]}");
-                var distance = localStations[i] - localStations[i+1];
-                if (distance > fullTankDistance)
+                if (i != stations.Count() - 1)
                 {
-                    throw new Exception();
-                }
-                result.Add(localStations[i] - localStations[i+1]);                
+                    var distance = stations[i+1] - stations[currentPoint];
+                    if(distance > maxDistance)
+                    {
+                        if(i != currentPoint)
+                        {
+                            stops++;
+                            var otherStops = CountStopsNumber(maxDistance, stations, i);
+                            if (otherStops == -1)
+                            {
+                                return -1;
+                            }
+                            stops += otherStops;
+                            break;
+                        }
+                        else
+                        {
+                            return -1;
+                        }
+                    }
+                }                
             }
-            return result;
-        }
-
-        static int CountStopsNumber(List<int> stations, int maxDistance)
-        {
-            var stopsCounter = 0;
-            var i = 0;
-            while(i < stations.Count())
-            {
-                var j = i;
-                var currentDistance = stations[j];
-                while(currentDistance<=maxDistance && j < stations.Count())
-                {
-                    j++;
-                    currentDistance += stations[j];
-                }
-                if (currentDistance > maxDistance)
-                {
-                    stopsCounter++;
-                }
-            };
-            return stopsCounter;
+            return stops;
         }
     }
 }
