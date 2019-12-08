947da19cd5783c1d52ac1fa2523a75c18875f3bb
Ilya.Kulikov <ilia.s.kulikov@gmail.com>
2019-12-08 11:18:00 +0300

    DataStructures Week2
    MakeHeap passed

--- DataStructures/week2_priority_queues_and_disjoint_sets/1_make_heap/MH.cs
+++ DataStructures/week2_priority_queues_and_disjoint_sets/1_make_heap/MH.cs
@@ -1,38 +1,41 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Text;
 
 namespace Heap
 {
     public static class Program
     {
         private static List<string> StringsToPrint { get; set; }
-        private static int SwapCounter { get; set; }
+        private static StringBuilder StringsToPrint2 { get; set; }
+        private static long SwapCounter { get; set; }
+
         public static void Main()
         {
-            var inputArrayLength = int.Parse(Console.ReadLine() ?? throw new ArgumentException());
-            var array = Console .ReadLine()?.Split(' ').Select(int.Parse).ToArray();
+            var inputArrayLength = long.Parse(Console.ReadLine());
+            var array = Console.ReadLine()?.Split(' ').Select(long.Parse).ToArray();
             CreateHeap(array);
         }
 
-        private static void CreateHeap(int[] array)
+        private static void CreateHeap(long[] array)
         {
             var elementsQuantity = array.Length;
             var startIndex = elementsQuantity / 2;
             SwapCounter = 0;
             StringsToPrint = new List<string>();
-            for (var i = startIndex; i < elementsQuantity; i++)
+            StringsToPrint2 = new StringBuilder(elementsQuantity);
+            for (var i = startIndex - 1; i >= 0; i--)
             {
-                SiftUp(array, i);
+                SiftDown(array, i);
             }
+
             Console.WriteLine(SwapCounter);
-            foreach (var s in StringsToPrint)
-            {
-                Console.WriteLine(s);
-            }
+            Console.Write(StringsToPrint2.ToString());
         }
 
-        private static void SiftUp(int[] array, int i)
+        private static void SiftUp(long[] array, long i)
         {
             while (true)
             {
@@ -45,7 +48,7 @@ namespace Heap
                 if (array[parentIndex] > array[i])
                 {
                     SwapCounter++;
-                    StringsToPrint.Add($"{parentIndex} {i}");
+                    StringsToPrint2.AppendLine($"{parentIndex} {i}");
                     var temp = array[parentIndex];
                     array[parentIndex] = array[i];
                     array[i] = temp;
@@ -57,7 +60,49 @@ namespace Heap
             }
         }
 
-        private static int GetParent(int i)
+        private static void SiftDown(long[] array, long i)
+        {
+            while (true)
+            {
+                var minIndex = i;
+                var l = GetLeftChildIndex(i);
+                if (l < array.Length && array[l] < array[minIndex])
+                {
+                    minIndex = l;
+                }
+
+                var r = GetRightChildIndex(i);
+                if (r < array.Length && array[r] < array[minIndex])
+                {
+                    minIndex = r;
+                }
+
+                if (minIndex != i)
+                {
+                    SwapCounter++;
+                    StringsToPrint2.AppendLine($"{minIndex} {i}");
+                    var temp = array[minIndex];
+                    array[minIndex] = array[i];
+                    array[i] = temp;
+                    i = minIndex;
+                    continue;
+                }
+
+                break;
+            }
+        }
+
+        private static long GetLeftChildIndex(long i)
+        {
+            return 2 * i + 1;
+        }
+
+        private static long GetRightChildIndex(long i)
+        {
+            return 2 * i + 2;
+        }
+
+        private static long GetParent(long i)
         {
             if (i != 0)
             {
@@ -68,7 +113,7 @@ namespace Heap
         }
 
 
-        private static bool IsHeap(int[] array)
+        private static bool IsHeap(long[] array)
         {
             var elementsQuantity = array.Length;
             for (var i = 0; i < elementsQuantity; i++)
@@ -81,13 +126,14 @@ namespace Heap
                         return false;
                     }
                 }
+
                 var r = l + 1;
                 if (r <= elementsQuantity - 1)
                 {
                     if (array[i] > array[r])
                     {
                         return false;
-                    }                    
+                    }
                 }
             }
 
