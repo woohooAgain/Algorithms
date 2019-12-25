faf2990185af0950d2f5a54584cfb0f9dd61e963
Ilya Kulikov <Ilya.Kulikov@comindware.com>
2019-12-25 22:22:36 +0300

    DataStructures Week2
    Add datastructures

--- DataStructures/week2_priority_queues_and_disjoint_sets/3_merging_tables/MT.cs
+++ DataStructures/week2_priority_queues_and_disjoint_sets/3_merging_tables/MT.cs
@@ -9,10 +9,72 @@ namespace DSA.Algorithms.Week2
         public static void Main()
         {
             var tablesAndQueries = Console.ReadLine()?.Split(' ').Select(long.Parse).ToList();
-            var tablesQuantity = threadAndJobs.First();
-            var queriesQuantity = threadAndJobs.Last();
-            var jobsDuration = Console.ReadLine()?.Split(' ').Select(long.Parse).ToArray();
-            Simulate(threadsQuantity, jobsDuration);
+            var tablesQuantity = tablesAndQueries.First();
+            var queriesQuantity = tablesAndQueries.Last();
+            var rowsSizes = Console.ReadLine()?.Split(' ').Select(long.Parse).ToArray();
+            var set = new MyDisjointSet(tablesQuantity);
+            foreach(var size in rowsSizes)
+            {
+                set.MakeSet(new Table(size));
+            }
+            var queries = new Query[queriesQuantity];
+            for(var i = 0; i < queriesQuantity; i++)
+            {
+                var pair = Console.ReadLine()?.Split(' ');
+                queries[i] = new Query(long.Parse(pair[1]), long.Parse(pair[0]));
+            }
+            Merge(set, queries);
         }
+
+        private static void Merge(MyDisjointSet set, Query[] queries)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Table
+    {
+        public Table(long rowsQuantity)
+        {
+            Rows = rowsQuantity;
+        }
+        public long Rows { get; set; }
+
+        public Table Parent { get; set; }
+    }
+
+    public class MyDisjointSet
+    {
+        public MyDisjointSet(long capacity)
+        {
+            Array = new Table[capacity];
+        }
+        Table[] Array { get; set; }
+
+        public void MakeSet(Table table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Union(Table tableA, Table tableB)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Table Find(Table table)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Query
+    {
+        public Query(long source, long destination)
+        {
+            Source = source;
+            Destination = destination;
+        }
+        public long Source { get; set; }
+        public long Destination { get; set; }
     }
 }
