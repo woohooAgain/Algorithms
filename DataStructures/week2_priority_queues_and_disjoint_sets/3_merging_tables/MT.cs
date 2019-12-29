43e0c7584eac02a7398dd7e05e7a2e48e470347a
Ilya.Kulikov <ilia.s.kulikov@gmail.com>
2019-12-29 20:50:59 +0300

    DataStructures Week2
    Half working code. Does not work for big auto test, works for simple tests from task description

--- DataStructures/week2_priority_queues_and_disjoint_sets/3_merging_tables/MT.cs
+++ DataStructures/week2_priority_queues_and_disjoint_sets/3_merging_tables/MT.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 
 namespace DSA.Algorithms.Week2
 {
@@ -8,62 +9,104 @@ namespace DSA.Algorithms.Week2
     {
         public static void Main()
         {
-            var tablesAndQueries = Console.ReadLine()?.Split(' ').Select(long.Parse).ToList();
-            var tablesQuantity = tablesAndQueries.First();
-            var queriesQuantity = tablesAndQueries.Last();
-            var rowsSizes = Console.ReadLine()?.Split(' ').Select(long.Parse).ToArray();
-            var set = new MyDisjointSet(tablesQuantity);
-            foreach(var size in rowsSizes)
+            using (var sr = new StreamReader(@"tests/116"))
             {
-                set.MakeSet(new Table(size));
-            }
-            var queries = new Query[queriesQuantity];
-            for(var i = 0; i < queriesQuantity; i++)
-            {
-                var pair = Console.ReadLine()?.Split(' ');
-                queries[i] = new Query(long.Parse(pair[1]), long.Parse(pair[0]));
-            }
-            Merge(set, queries);
+                var tablesAndQueries = sr.ReadLine()?.Split(' ').Select(long.Parse).ToList();
+                var tablesQuantity = tablesAndQueries.First();
+                var queriesQuantity = tablesAndQueries.Last();
+                var rowsSizes = sr.ReadLine()?.Split(' ').Select(long.Parse).ToArray();
+                var set = new MyDisjointSet(tablesQuantity, rowsSizes);
+                var queries = new Query[queriesQuantity];
+                for(var i = 0; i < queriesQuantity; i++)
+                {
+                    var pair = sr.ReadLine()?.Split(' ');
+                    queries[i] = new Query(long.Parse(pair[1]), long.Parse(pair[0]));
+                }
+                Merge(set, queries);
+            }            
         }
 
         private static void Merge(MyDisjointSet set, Query[] queries)
         {
-            throw new NotImplementedException();
+            using (var sw = new StreamWriter(@"tests/116.a"))
+            {
+                foreach (var query in queries)
+                {
+                    set.Union(query.Destination - 1, query.Source - 1);
+                    sw.WriteLine(set.GetLongestTableSize());
+                }
+            }            
         }
     }
 
     public class Table
     {
-        public Table(long rowsQuantity)
+        public Table(long rowsQuantity, long arrayIndex)
         {
             Rows = rowsQuantity;
+            Parent = arrayIndex;
         }
         public long Rows { get; set; }
 
-        public Table Parent { get; set; }
+        public long Parent { get; set; }
     }
 
     public class MyDisjointSet
     {
-        public MyDisjointSet(long capacity)
+        //todo
+        //add heuristics for rank and find
+        //measure performance
+        
+        public MyDisjointSet(long capacity, long[] rowsSizes)
         {
             Array = new Table[capacity];
+            LongestTableSize = long.MinValue;
+            for (var i = 0; i < capacity; i++)
+            {
+                if (rowsSizes[i] > LongestTableSize)
+                {
+                    LongestTableSize = rowsSizes[i];
+                }
+                Array[i] = new Table(rowsSizes[i], i);
+            }
         }
         Table[] Array { get; set; }
+
+        private long LongestTableSize { get; set; }
 
         public void MakeSet(Table table)
         {
             throw new NotImplementedException();
         }
 
-        public void Union(Table tableA, Table tableB)
+        public void Union(long destinationTableIndex, long sourceTableIndex)
         {
-            throw new NotImplementedException();
+            if (destinationTableIndex != sourceTableIndex)
+            {
+                var destination = Find(destinationTableIndex);
+                var source = Find(sourceTableIndex);
+                source.Parent = destination.Parent;
+                destination.Rows += source.Rows;
+                source.Rows = 0;
+                if (destination.Rows > LongestTableSize)
+                {
+                    LongestTableSize = destination.Rows;
+                }
+            }
         }
 
-        public Table Find(Table table)
+        public Table Find(long index)
         {
-            throw new NotImplementedException();
+            while (true)
+            {
+                if (Array[index].Parent == index) return Array[index];
+                index = Array[index].Parent;
+            }
+        }
+
+        public long GetLongestTableSize()
+        {
+            return LongestTableSize;
         }
     }
 
